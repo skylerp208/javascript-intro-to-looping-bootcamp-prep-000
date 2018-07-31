847ac2f33f1e7bdd3d87068e365ac7334fbcf1e0
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-07-31 05:55:24 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,10 @@
 function forLoop(array) {
   for (let i = 1; i <25; i++) {
-    
+    if (i === 1) {
+      + "I am 1 strange loop."}
+      else {
+        + `I am ${i} strange loops.`}
+      }
+    }
   }
 }
